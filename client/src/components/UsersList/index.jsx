2071a2155084986fe4cbf76a283e8b0c94c2cbaa
Viktoriia <pecourses>
2023-04-23 16:17:17 +0300

    Add users fetching

--- client/src/components/UsersList/index.jsx
+++ client/src/components/UsersList/index.jsx
@@ -2,15 +2,31 @@ import { connect } from 'react-redux';
 import BeatLoader from 'react-spinners/BeatLoader';
 import styles from './UsersList.module.sass';
 import defImage from './defaultPhoto.jpg';
+import { useEffect, useState } from 'react';
 
-export const UsersList = ({ users, isFetching, error }) => {
+export const UsersList = ({ isFetching, error }) => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/api/users')
+      .then(response => response.json())
+      .then(values => setUsers(values.data));
+  }, []);
+
   return (
     <>
       <BeatLoader loading={isFetching} />
       {error && <div>!!!ERROR!!!</div>}
       <ul>
         {users.map(u => (
-          <li key={u.id}>{JSON.stringify(u)}</li>
+          <li key={u.id}>
+            <img
+              className={styles.userImage}
+              src={u.image ? `http://localhost:5000/${u.image}` : defImage}
+              alt={`${u.firstName} ${u.lastName}`}
+            />
+            {JSON.stringify(u)}
+          </li>
         ))}
       </ul>
     </>
